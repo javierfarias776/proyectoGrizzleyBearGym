5300db3c9f483afb6afcac65053754bf19146aa4
javierfarias776 <edwincarrilo.776@gmail.com>
2024-02-07 13:44:14 -0300

    subida de formulario

---
+++ index.js
@@ -0,0 +1,10 @@
+let boton= document.getElementById("btnbtn");
+boton.addEventListener("click", () => {
+    Swal.fire({
+        title: "Excelente",
+        text: "Tus datos se han enviado correctamente!",
+        text: "Acercate a tu cede GrizzleyBearGym mas cercana para hacer el pago",
+        icon: "success"
+      });
+
+})
